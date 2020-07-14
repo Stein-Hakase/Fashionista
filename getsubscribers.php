982b377ce0d330534c3496c6447a05aef551dd2f
Stein-Hakase <fakhriddine.hariz@gmail.com>
2020-07-14 19:07:19 +0700

    styling the  liste of subscriber , adding style to front page and orginazing repository

--- getsubscribers.php
+++ getsubscribers.php
@@ -3,19 +3,38 @@ include_once 'connect.php';
 // *******
 
 $sql= "SELECT * FROM heroku_40f9d5d0300dd93.subscribers ;";
+$count=0;
 // $sql= "SELECT * FROM subscribers ;";
-if($result = mysqli_query($conn, $sql)){
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/bootstrap.css">
+    <title>Document</title>
+</head>
+<body>
+ <?php
+ if($result = mysqli_query($conn, $sql)){
     if(mysqli_num_rows($result) > 0){
-        echo "<table>";
+        echo "<table class='table'>";
             echo "<tr>";
-                echo "<th>Full Name</th>";
-                echo "<th>E-mail</th>";
+                echo"<th scope='col'>#</th>";
+                echo "<th scope='col' >Full Name</th>";
+                echo "<th scope='col'>E-mail</th>";
             echo "</tr>";
+
         while($row = mysqli_fetch_array($result)){
+            
+           if ( $row['Email']!='' ){
+            $count=$count+1;
             echo "<tr>";
+                echo "<th scope='row'>". $count . "</th>";
                 echo "<td>" . $row['FullName'] . "</td>";
                 echo "<td>" . $row['Email'] . "</td>";
-            echo "</tr>";
+            echo "</tr>";}
         }
         echo "</table>";
         // Free result set
@@ -27,7 +46,7 @@ if($result = mysqli_query($conn, $sql)){
     echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
 
 } 
-// header('location: get.php')
-?>
-
+ ?>   
+</body>
+</html>
 
---
+++ js/custom-js/subscriptionHandler.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+  $('form').submit(function(event){
+    event.preventDefault()
+    const data ={
+        name :event.target.name.value ,
+        email:event.target.email.value ,
+
+    }
+        var fd = new FormData();
+        //very simply, doesn't handle complete objects
+        for(var i in data){
+        fd.append(i,data[i]);
+        }
+        let addsubscribe = async () =>{
+        let post = await fetch('postsubscriber.php', {
+        method: 'POST',
+        body:fd,
+        mode:"cors",
+        }).then(function (response) {
+            if (response.ok) {
+                alert('Congratulations , your mail was saved  !! ')
+                $( '.subscribers-link' ).css( "visibility", "visible" )
+            } else {
+                console.error('Something went wrong');
+            }
+        })
+       
+     }
+      
+        if (data.email){
+            addsubscribe()
+        }else alert('please enter your e-mail ')
+  })
+
+  });
