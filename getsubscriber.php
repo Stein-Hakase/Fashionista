20b624e0a841971cae657f45207a286028eb937a
Stein-Hakase <fakhriddine.hariz@gmail.com>
2020-07-14 11:30:05 +0700

    testing db on heroku

--- getsubscriber.php
+++ getsubscriber.php
@@ -1,8 +1,8 @@
 <?php
-$dbServername='localhost';
-$dbUsername='root';
-$dbPwd='';
-$dbName='landingpage';
+$dbServername='eu-cdbr-west-03.cleardb.net';
+$dbUsername='bd1a4db291e884';
+$dbPwd='c6e71ad4';
+$dbName='heroku_40f9d5d0300dd93';
 $conn = mysqli_connect($dbServername , $dbUsername ,$dbPwd,$dbName);
 // *******
 
